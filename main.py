701b6dca9c9b2ead2155a82a501add618653a300
Maxim <maksapanovich.n@mail.ru>
2023-02-14 23:27:04 +0300

    test
    test

---
+++ main.py
@@ -0,0 +1,41 @@
+from tkinter import *
+
+def show_table():
+    num = num_box.get()
+    num = int(num)
+    value = 1
+    for i in range(1, 13):
+        answer = i * num
+        num_list.insert(END, (i, "x", num, "=", answer))
+        value += 1
+    num_box.delete(0, END)
+    num_box.focus()
+
+
+def clear_list():
+    num_box.delete(0, END)
+    num_list.delete(0, END)
+    num_box.focus()
+
+
+window = Tk()
+window.title('Таблица умножения ')
+window.geometry("400x280")
+
+label1 = Label(text = "Введите число")
+label1.place(x = 20, y = 20, width= 100, height= 25)
+
+num_box = Entry(text = 0)
+num_box.place(x = 120, y = 20, width= 100, height= 25)
+num_box.focus()
+
+button1 = Button(text= 'Подтвердить', command= show_table)
+button1.place(x = 250, y = 20, width= 120, height= 25)
+
+num_list = Listbox()
+num_list.place(x = 120, y = 50, width= 100, height= 200)
+
+button2 = Button(text= 'Удалить', command= clear_list)
+button2.place(x = 250, y = 50, width= 120, height= 25)
+
+window.mainloop()
